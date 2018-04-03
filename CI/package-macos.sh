b180f8b8d21af5f5890febef5a6a5c2a6fdf482b
Will Townsend <will@townsend.io>
2018-04-04 11:11:45 +1200

    Fix path reference to ffmpeg dynamic library, use @rpath instead.

--- CI/package-macos.sh
+++ CI/package-macos.sh
@@ -17,6 +17,12 @@ fi
 export FILENAME="obs-ios-camera-source-$VERSION.pkg"
 export LATEST_FILENAME="obs-ios-camera-source-latest-$LATEST_VERSION.pkg"
 
+echo "-- Modifying obs-ios-camera-source.so"
+install_name_tool \
+	-change /usr/local/opt/ffmpeg/lib/libavcodec.57.dylib @rpath/libavcodec.57.dylib \
+	-change /usr/local/opt/ffmpeg/lib/libavutil.55.dylib @rpath/libavutil.55.dylib \
+	./build/obs-ios-camera-source.so
+
 echo "-- Dependencies for obs-ios-camera-source"
 otool -L ./build/obs-ios-camera-source.so
 
