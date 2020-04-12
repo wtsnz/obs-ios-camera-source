51f2340f2cb51b5b4bcf9402c9b20206b3dd4394
Will Townsend <will@townsend.io>
2020-04-11 23:18:13 -0700

    Update CI (#41)
    * Update CI
    
    All credit to the obs-ndi project. Without that, I wouldn't have been able to do this.
    https://github.com/Palakis/obs-ndi
    
    * fixup! Update CI
    
    * fixup! Update CI
    
    * fixup! Update CI
    
    * fixup! Update CI
    
    * fixup! Update CI
    
    * fixup! Update CI

--- CI/build-macos.sh
+++ CI/build-macos.sh
@@ -18,10 +18,11 @@ echo "[obs-ios-camera-plugin] Building 'obs-ios-camera-plugin' for macOS."
 
 mkdir build && cd build
 cmake .. \
-  -DDepsPath=/tmp/obsdeps \
-  -DQTDIR=/usr/local/opt/qt \
-  -DLIBOBS_INCLUDE_DIR=../../obs-studio/libobs \
-  -DLIBOBS_LIB=../../obs-studio/libobs \
-  -DOBS_FRONTEND_LIB="$(pwd)/../../obs-studio/build/UI/obs-frontend-api/libobs-frontend-api.dylib" \
-  -DCMAKE_INSTALL_PREFIX=/usr \
+  -DCMAKE_OSX_DEPLOYMENT_TARGET=10.11 \
+	-DQTDIR=/usr/local/opt/qt \
+	-DLIBOBS_INCLUDE_DIR=../../obs-studio/libobs \
+	-DLIBOBS_LIB=../../obs-studio/libobs \
+	-DOBS_FRONTEND_LIB="$(pwd)/../../obs-studio/build/UI/obs-frontend-api/libobs-frontend-api.dylib" \
+	-DCMAKE_BUILD_TYPE=RelWithDebInfo \
+	-DCMAKE_INSTALL_PREFIX=/usr \
 && make -j4
