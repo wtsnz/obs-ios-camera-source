79317aa070f7a6772362149825944fbeab90f61c
Will Townsend <will@townsend.io>
2021-01-14 14:22:30 -0800

    Increment version

--- src/obs-ios-camera-plugin.cpp
+++ src/obs-ios-camera-plugin.cpp
@@ -22,7 +22,7 @@
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE("obs-ios-camera-plugin", "en-US")
 
-#define IOS_CAMERA_PLUGIN_VERSION "2.7.0"
+#define IOS_CAMERA_PLUGIN_VERSION "2.7.1"
 
 extern void RegisterIOSCameraSource();
 
