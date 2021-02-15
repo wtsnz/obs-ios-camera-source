4157b0727740d8f10b554575cc2952bcbbb9ba07
Will Townsend <will@townsend.io>
2021-02-15 12:44:00 -0800

    Reset cache

--- CI/install-ios-camera-dependencies.cmd
+++ CI/install-ios-camera-dependencies.cmd
@@ -1,15 +1,3 @@
-REM if not exist %OpenSSLBaseDir% (
-REM 	curl -kLO https://curl.se/windows/dl-7.75.0/openssl-1.1.1i-win32-mingw.zip -f --retry 5 -z openssl-1.1.1i-win32-mingw.zip
-REM     7z x openssl-1.1.1i-win32-mingw.zip -o%OpenSSLBaseDir%
-
-REM     curl -kLO https://curl.se/windows/dl-7.75.0/openssl-1.1.1i-win64-mingw.zip -f --retry 5 -z openssl-1.1.1i-win64-mingw.zip
-REM     7z x openssl-1.1.1i-win64-mingw.zip -o%OpenSSLBaseDir%
-REM ) else (
-REM 	echo "OpenSSL is already installed. Download skipped."
-REM )
-
-REM dir %OpenSSLBaseDir%
-
 cd c:\vcpkg
 vcpkg integrate install
 
